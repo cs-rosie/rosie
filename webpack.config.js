6d39bc2f9fd14d5c128382b8ff728b064d45c9af
Ben Gummelt <camaromelt@gmail.com>
2018-05-16 15:11:27 -0700

    Added Dist Folder and Updated Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,40 @@
 const path = require('path');
 
 module.exports = {
-    mode:'none',
-    entry: './index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    module:{
-        rules: [
-            {
-                test:/\.jsx?$/,
-                exclude:/node_modules/,
-                loader:'babel-loader',
-                query:{
-                    presets:['es2015','react']
-                }
-            } ,
-            {
-                test: /\.css$/,
-                use:[
-                    {
-                        loader:'style-loader'
-                    },
-                    {
-                        loader:'css-loader'
-                    }
-                ]
-            }
+  mode: 'none',
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules:
+    [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+          }
+      },
+      {
+        test: /\.css$/,
+        use:
+        [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
         ]
-    },
-
-    devServer:{
-        publicPath: "/build/",
-        hot: true,
-        port: 8080
-    }
-
-
-
-
-
+      }
+    ]
+  },
+  devServer: {
+    publicPath: '/dist',
+    hot: true,
+    port: 8080
+  }
 }
