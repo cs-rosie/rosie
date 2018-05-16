45dd06303e1ed7516ae00d3dcfdb3b4959c7a7ad
pskchang <pschang@ucsd.edu>
2018-05-16 00:26:06 -0700

    Set up base files for server, moved from soundrip

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const authController = require('./controllers/authController');
+const calController = require('./controllers/calController');
+const slackController = require('./controllers/slackController');
+const PORT = 8080;
+
+// const db = `postgres://icgdoqir:DS6zvF0G1MJijrBwz0y7UvGdt55t0x5x@pellefant.db.elephantsql.com:5432/icgdoqir`;
+const app = express();
+
+app.use('/vote', vote);
+app.use('/song', song);
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Home page ok');
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}. Go to /login to initiate spotify oAuth`));
