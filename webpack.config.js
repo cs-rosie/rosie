1fa1a31300f0ca93b23d920dd8bd2471888835d7
Jonathan Ko <jonathanko@Jonathans-MacBook-Pro.local>
2018-05-16 11:54:06 -0700

    JK initial push

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+module.exports = {
+    mode:'none',
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module:{
+        rules: [
+            {
+                test:/\.jsx?$/,
+                exclude:/node_modules/,
+                loader:'babel-loader',
+                query:{
+                    presets:['es2015','react']
+                }
+            } ,
+            {
+                test: /\.css$/,
+                use:[
+                    {
+                        loader:'style-loader'
+                    },
+                    {
+                        loader:'css-loader'
+                    }
+                ]
+            }
+        ]
+    },
+
+    devServer:{
+        publicPath: "/build/",
+        hot: true,
+        port: 8080
+    }
+
+
+
+
+
+}
