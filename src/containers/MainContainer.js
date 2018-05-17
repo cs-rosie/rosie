00d8b290e4b4472e1841489c0aa49b9fd7ee8a81
Ben Gummelt <camaromelt@gmail.com>
2018-05-17 14:49:51 -0700

    Updated Appointment Pages.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as actionCreators from '../Actions/actionCreators';
+import AppointmentContainer from './AppointmentContainer';
 
 const mapStateToProps = store => ({
 });
@@ -22,7 +23,7 @@ class MainContainer extends Component {
   render() {
     return (
       <div id="Events">
-        I am here!!!
+        <AppointmentContainer />
       </div> 
     );
   }
